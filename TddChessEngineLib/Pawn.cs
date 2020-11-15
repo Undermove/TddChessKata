4b798aded84f22284ac6fd5cf8a61306698cebd1
Dmitry Afonchenko <undermove@yandex.ru>
2020-11-15 12:04:59 +0300

    Create turn method with tests

--- TddChessEngineLib/Pawn.cs
+++ TddChessEngineLib/Pawn.cs
@@ -4,6 +4,23 @@ namespace TddChessEngineLib
 {
     public class Pawn
     {
-        public void Turn(object startPosition, object finishPosition){}
+        public string CurrentPosition { get; private set; }
+
+        public Pawn(string initialPosition)
+        {
+            CurrentPosition = initialPosition;
+        }
+
+        public void Turn(string startPosition, string finalPosition)
+        {
+            if(startPosition == CurrentPosition)
+            {
+                CurrentPosition = finalPosition;
+            }
+            else
+            {
+                throw new ArgumentException($"Current position is {CurrentPosition} not {startPosition}");
+            }
+        }
     }
 }
--- TddChessEngineLibTests/PawnTests.cs
+++ TddChessEngineLibTests/PawnTests.cs
@@ -9,6 +9,13 @@ namespace TddChessEngineLibTests
         [Fact]
         public void WhenPawnTurnsFromE2ToE4_ThenItsPositionsChanges()
         {
+            string startPosition = "E2";
+            string finalPosition = "E4";
+            Pawn pawn = new Pawn(startPosition);
+
+            pawn.Turn(startPosition, finalPosition);
+
+            Assert.Equal(finalPosition, pawn.CurrentPosition);
         }
     }
 }
