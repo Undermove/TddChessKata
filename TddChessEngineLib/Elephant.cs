2f0a34ebba8980c7244c5b802196a74607d14e41
d.afonchenko <d.afonchenko@dodopizza.com>
2020-12-08 13:33:38 +0300

    add elephant

---
+++ TddChessEngineLib/Elephant.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace TddChessEngineLib
+{
+    public class Elephant
+    {
+
+    }
+}
---
+++ TddChessEngineLibTests/ElephantTests.cs
@@ -0,0 +1,19 @@
+using Xunit;
+
+namespace TddChessEngineLibTests
+{
+    public class ElephantTests
+    {
+        [Fact]
+        public void WhenCreatingElephantOnE2_ThenItCurrentPositionE2()
+        {
+
+        }
+
+        [Fact]
+        public void WhenCreatingElephantOnE9_ThenItThrowsArgumentException()
+        {
+
+        }
+    }
+}
