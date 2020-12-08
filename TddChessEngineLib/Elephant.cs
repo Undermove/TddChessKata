bf047ae11fe0bfe1dd93ce65548c93ccc80a87e2
d.afonchenko <d.afonchenko@dodopizza.com>
2020-12-08 13:53:40 +0300

    Add elephant task

--- TddChessEngineLib/Elephant.cs
+++ TddChessEngineLib/Elephant.cs
@@ -4,6 +4,18 @@ namespace TddChessEngineLib
 {
     public class Elephant
     {
+        public string CurrentPosition {get;set;}
 
+        // тут модифицировать так чтобы тесты проходили
+        public Elephant(string position)
+        {
+            
+        }
+
+        // для этого метода написать тест
+        public void Turn(string newPosition)
+        {
+
+        }
     }
 }
--- TddChessEngineLibTests/ElephantTests.cs
+++ TddChessEngineLibTests/ElephantTests.cs
@@ -1,19 +1,76 @@
+using System;
 using Xunit;
+using TddChessEngineLib;
 
 namespace TddChessEngineLibTests
 {
     public class ElephantTests
     {
+        // поправить тест, чтобы он заработал
         [Fact]
-        public void WhenCreatingElephantOnE2_ThenItCurrentPositionE2()
+        public void JustCreatingElephantTest()
         {
+            // Arrange
+            // Act
+            // создаём слона сейчас этот код просто не работает и подчёркнут красным, это нужно исправить
+            Elephant elephant = new Elephant(); 
 
+            //  Assert
+            // проверяем что он создался
+            Assert.NotNull(elephant);
         }
 
         [Fact]
-        public void WhenCreatingElephantOnE9_ThenItThrowsArgumentException()
+        public void WhenCreatingElephantOnC1_ThenItCurrentPositionOnC1()
         {
+            // Arrange
+            // задаём слону стартовую позицию
+            string elephantPosition = "C1";
 
+            // Act
+            // создаём слона на стартовой позиции
+            Elephant elephant = new Elephant(elephantPosition);
+
+            // Assert
+            // проверяем, что позиция применилась
+            Assert.Equal(elephantPosition, elephant.CurrentPosition);
+        }
+
+        [Fact]
+        public void WhenCreatingElephantOnBBB_ThenItThrowsArgumentException()
+        {
+            // Arrange
+            // задаём слону стартовую позицию
+            string elephantIncorrectPosition = "BBB";
+
+            // Act and Assert
+            // проверяем, что конструктор выкидывает ошибку, если клетка не C1
+            Assert.Throws<ArgumentException>(() => new Elephant(elephantIncorrectPosition));
+        }
+
+        [Fact]
+        public void WhenElephantTurnsFromC1ToD2_ThenItChangesCurrentPosition()
+        {
+            // Arrange
+            // задаём слону стартовую позицию
+            string elephantStartPosition = "C1";
+            string elephantEndPosition = "D2";
+            Elephant elephant = new Elephant(elephantStartPosition);
+
+            // Act and
+            elephant.Turn(elephantEndPosition);
+
+            //Assert
+            // проверяем, что конструктор выкидывает ошибку, если клетка не C1
+            Assert.Equal(elephantEndPosition, elephant.CurrentPosition);
+        }
+
+        [Fact]
+        public void WhenElephantTurnsFromC1ToBBB_ThenItThrowsArgumentException()
+        {
+            // усложнённая задача
+            // написать тест, который проверяет случай, когда слон пытается пойти на клетку, которой не существует BBB
+            // в этом случае метод Turn кидает исключение
         }
     }
 }
